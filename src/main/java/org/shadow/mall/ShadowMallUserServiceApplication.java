74ff61888b205502746023f3e275090f9c9e44e8
jianjustin <janine.jian.chen@gmail.com>
2020-05-28 20:10:30 +0800

    配置服务注册

--- src/main/java/org/shadow/mall/ShadowMallUserServiceApplication.java
+++ src/main/java/org/shadow/mall/ShadowMallUserServiceApplication.java
@@ -2,8 +2,10 @@ package org.shadow.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShadowMallUserServiceApplication {
 
 	public static void main(String[] args) {
